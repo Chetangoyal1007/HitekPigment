1e4431ecc68623cfbc77f5cceeca958e495ee741
Chetangoyal1007 <chetan1457.be22@chitkara.edu.in>
2025-12-18 20:30:24 +0530

    Update phone number in Navbar component for accuracy

--- hitek pigment/src/components/Navbar/Navbar.jsx
+++ hitek pigment/src/components/Navbar/Navbar.jsx
@@ -74,7 +74,7 @@ const Navbar = () => {
 
   {/* Number ONLY on desktop */}
   <span className="hidden sm:inline text-base font-medium whitespace-nowrap">
-    +91 7009406565
+    +91 9417604141
   </span>
 </a>
 
